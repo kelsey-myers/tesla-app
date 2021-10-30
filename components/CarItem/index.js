f65082b330e1d54d592a5447bfe9e1eb05584944
Kelsey Myers <52179263+kelsey-myers@users.noreply.github.com>
2021-10-30 14:38:10 +0100

    First commit

---
+++ components/CarItem/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, Text, ImageBackground } from "react-native";
+import styles from "./styles";
+
+export default function CarItem() {
+  return (
+    <View style={styles.carContainer}>
+      <ImageBackground
+        source={require("../../assets/images/ModelX.jpeg")}
+        style={styles.image}
+      />
+      <View style={styles.titles}>
+        <Text style={styles.title}>Model S</Text>
+        <Text style={styles.subTitle}>Starting at £69,420</Text>
+      </View>
+    </View>
+  );
+}
